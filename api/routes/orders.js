c970d216f70ac5eb711f3f3d709d7ede127fee05
@ivelton2018 <ivelton2018@gmail.com>
2018-10-24 17:41:15 -0400

    express app

---
+++ api/routes/orders.js
@@ -0,0 +1,36 @@
+const express = require('express')
+const router = express.Router()
+
+router.get('/', (req, res, next) => {
+	res.status(200).json({
+		message: 'GET requests /orders'
+	})
+})
+
+router.post('/', (req, res, next) => {
+	res.status(201).json({
+		message: 'POST requests /orders'
+	})
+})
+
+router.get('/:orderId', (req, res, next) => {
+	const id = req.params.orderId
+	res.status(200).json({
+		message: 'GET order',
+			id: id
+	})
+})
+
+router.patch('/:orderId', (req, res, next) => {
+	res.status(200).json({
+		message: 'Update order'
+	})
+})
+
+router.delete('/:orderId', (req, res, next) => {
+	res.status(200).json({
+		message: 'Deleted order'
+	})
+})
+
+module.exports = router
