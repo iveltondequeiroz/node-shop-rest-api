f4a353b67b8d5f24ddaa8eee0b497afed9e5808b
@ivelton2018 <ivelton2018@gmail.com>
2018-10-29 17:25:18 -0400

    error handling added

--- app.js
+++ app.js
@@ -11,4 +11,20 @@ app.use(logger('dev'))
 app.use('/products', productRoutes)
 app.use('/orders', ordersRoutes)
 
+//handel erros
+app.use((req, res, next) => {
+	const error = new Error('Page Not Found')
+	error.status = 404
+	next(error)
+})
+
+app.use((error, req, res, next) => {
+	res.status(error.status || 500)
+	res.json({
+		error: {
+			message: error.message
+		}
+	})
+})
+
 module.exports = app
