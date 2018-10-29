bd55b35c45b1fb8a8525e4bfcbb9e4cdcd998077
@ivelton2018 <ivelton2018@gmail.com>
2018-10-29 17:51:37 -0400

    body parsing

--- app.js
+++ app.js
@@ -2,16 +2,21 @@ const express = require('express')
 const app = express()
 
 const logger = require('morgan')
+const bodyParser = require('body-parser')
 
 const productRoutes = require('./api/routes/products')
 const ordersRoutes = require('./api/routes/orders')
 
+// middlewares
 app.use(logger('dev'))
+app.use(bodyParser.urlencoded({extended: false}))
+app.use(bodyParser.json())
 
+// routes
 app.use('/products', productRoutes)
 app.use('/orders', ordersRoutes)
 
-//handel erros
+//errors handling
 app.use((req, res, next) => {
 	const error = new Error('Page Not Found')
 	error.status = 404
