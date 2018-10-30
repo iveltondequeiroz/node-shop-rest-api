05d5a4bb41877a2430802cfd3336666b968b9c84
@ivelton2018 <ivelton2018@gmail.com>
2018-10-29 21:08:52 -0400

    PATCH product props

--- api/routes/products.js
+++ api/routes/products.js
@@ -6,9 +6,18 @@ const Product = require('../models/product')
 
 
 router.get('/', (req, res, next) => {
-	res.status(200).json({
-		message: 'GET requests /products'
-	})
+	Product.find()
+		.exec()
+		.then(docs => {
+			console.log('docs:', docs)
+			res.status(200).json(docs)	
+		})
+		.catch(err => {
+			console.log(err)
+			res.status(500).json({
+				error: err
+			})
+		})
 })
 
 router.post('/', (req, res, next) => {
@@ -55,15 +64,42 @@ router.get('/:productId', (req, res, next) => {
 })
 
 router.patch('/:productId', (req, res, next) => {
-	res.status(200).json({
-		message: 'Update product'
-	})
+	const id = req.params.productId
+	const updateOps = {}
+	for(const ops of req.body) {
+		updateOps[ops.propName] = ops.value
+	}
+	Product.update({ _id: id }, { $set: updateOps })
+		.exec()
+		.then(result =>{
+			console.log('patch result', result)
+			res.status(200).json({
+				message: result
+			})
+		})
+		.catch(err => {
+			console.log('patch err', err)
+			res.status(500).json({
+				error: err
+			})
+		})
 })
 
 router.delete('/:productId', (req, res, next) => {
-	res.status(200).json({
-		message: 'Deleted product'
-	})
+	const id = req.params.productId
+	Product.remove({_id:id})
+		.exec()
+		.then(result => {
+			res.status(200).json({
+				message: result
+			})
+		})
+		.catch(err => {
+			console.log(err)
+			res.status(500).json({
+				error: err
+			})
+		})
 })
 
 
